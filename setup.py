8d82df0003ce7a5cebac3a6e5452184d1ed492fd
Yan <yans@yancomm.net>
2016-01-23 13:14:00 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='worker',
+    version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
+    packages=[ 'worker', 'worker.workers' ],
+    install_requires=[
+    ],
+    description='Worker component of the Shellphish CRS.',
+    url='https://git.seclab.cs.ucsb.edu/cgc/miester',
+)
