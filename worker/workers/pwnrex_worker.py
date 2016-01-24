ec2aba10688c63114b2692ac3c75951299236210
JinBlack <jinblack17@gmail.com>
2016-01-24 14:20:24 -0800

    worker init

---
+++ worker/workers/pwnrex_worker.py
@@ -0,0 +1,24 @@
+from ..worker import Worker
+import crscommon
+import pwnrex
+
+import logging
+l = logging.getLogger('crs.worker.workers.pwnrex_worker')
+l.setLevel('DEBUG')
+
+
+class PwnrexWorker(Worker):
+    def __init__(self):
+        self._job = None
+
+    def _run(self, job):
+        '''
+        Runs pwnrex.
+        '''
+
+        # TODO: handle the possibility of a job submitting a PoV, rex already
+        # supports this
+        pwnrex.Pwnrex(job.binary.path, job.pcaPs)
+
+    def run(self, job):
+        self._run(job)
