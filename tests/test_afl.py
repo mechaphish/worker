fb335e082f463396dfd6bdf1e724cf3600359a65
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-12 19:27:56 -0800

    Fix AFLWorker tests

--- tests/test_afl.py
+++ tests/test_afl.py
@@ -1,28 +1,37 @@
 import os
-import worker, crscommon
-from farnsworth_client.models import Job, ChallengeBinaryNode
 import timeout_decorator
+import mock
+from nose.tools import *
 
-def test_ccf3d301():
-    aw = worker.AFLWorker()
-    ### FIXME: nedd to use a mock or factory library
-    aj = Job(id='xxx', worker='afl', cbn_id='xxx', limit_cpu=4, limit_memory=1, payload=None)
-    aj._cbn = ChallengeBinaryNode(id='xxx', name='ccf3d301_01', blob=None)
-    aj._cbn._binary_path = os.path.join('../cbs/qualifier_event/ccf3d301', 'ccf3d301_01')
-    aj._cbn._tests = []
-    ###
+import worker
+from farnsworth_client.models import Job, ChallengeBinaryNode
 
-    @timeout_decorator.timeout(30)
-    def _timeout_run():
-        aw.run(aj)
+class TestAFLWorker:
+    def setup(self):
+        self.cbn = mock.create_autospec(
+            ChallengeBinaryNode,
+            tests = [],
+            binary_path = os.path.join('../cbs/qualifier_event/ccf3d301', 'ccf3d301_01')
+        )
+        self.job = mock.create_autospec(
+            Job,
+            limit_cpu = 4,
+            limit_memory = 1,
+            cbn = self.cbn
+        )
+        self.aw = worker.AFLWorker()
 
-    try:
-        _timeout_run()
-    except timeout_decorator.TimeoutError:
-        pass
+    def test_it_finds_cases_and_assigns_them_to_cbn_tests(self):
+        @timeout_decorator.timeout(10)
+        def _timeout_run():
+            self.aw.run(self.job)
+        try:
+            _timeout_run()
+        except timeout_decorator.TimeoutError:
+            pass
 
-    print "Found %d crashes" % len(aw._fuzzer.crashes())
-    assert len(aj.binary.crashes) > 0
-
-if __name__ == '__main__':
-    test_ccf3d301()
+        cases = (self.aw._fuzzer.crashes() + self.aw._fuzzer.queue())
+        assert_greater(cases, 0)
+        assert_equals(len(self.aw._seen), len(self.job.cbn.tests))
+        # we check fuzzer results every 5s, so there could be less results in _seen
+        assert_greater_equal(len(cases), len(self.job.cbn.tests))
