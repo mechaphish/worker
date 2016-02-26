7a35eb1ca951410e95023b96b8f205fe5a25d902
Francesco Disperati <francesco@cs.ucsb.edu>
2016-02-25 19:02:52 -0800

    Setup CI tests

--- setup.py
+++ setup.py
@@ -5,18 +5,7 @@ setup(
     version='0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1',
     packages=[ 'worker', 'worker.workers' ],
     scripts=['bin/worker'],
-    install_requires=[
-        'python-dotenv==0.3.0',
-        'farnsworth_client',
-        'fuzzer',
-        'rex',
-        # test dependencies
-        'timeout-decorator',
-        'mock>=1.3.0',
-        'nose>=1.3.7',
-        'nose-timer>=0.5.0',
-        'coverage>=4.0.3'
-    ],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i],
     description='Worker component of the Shellphish CRS.',
     url='https://git.seclab.cs.ucsb.edu/cgc/miester',
 )
