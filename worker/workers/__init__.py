85878f0cf9917bce5c9077f19da887a27e71595b
Francesco Disperati <francesco@cs.ucsb.edu>
2016-03-28 17:48:32 -0700

    Temp disable Patcherex, waiting for a fix

--- worker/workers/__init__.py
+++ worker/workers/__init__.py
@@ -1,4 +1,4 @@
 from .afl_worker import AFLWorker
 from .rex_worker import RexWorker
 from .driller_worker import DrillerWorker
-from .patcherex_worker import PatcherexWorker
+# from .patcherex_worker import PatcherexWorker # FIXME
