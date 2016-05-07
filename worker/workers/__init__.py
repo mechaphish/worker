d2b8c2d276b0a616fa8f3416cb5ac9c83ae95bb4
Francesco Disperati <francesco@cs.ucsb.edu>
2016-05-06 18:04:00 -0700

    Re-enable patcherex worker

--- worker/workers/__init__.py
+++ worker/workers/__init__.py
@@ -1,4 +1,4 @@
 from .afl_worker import AFLWorker
 from .rex_worker import RexWorker
 from .driller_worker import DrillerWorker
-# from .patcherex_worker import PatcherexWorker # FIXME
+from .patcherex_worker import PatcherexWorker
