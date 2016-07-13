935a736b30b833b60c05074c3cc3046ad7582faf
Kevin Borgolte <cao@cao.vc>
2016-07-12 23:20:01 -0700

    Add missing arguments snapshot and restrict_net

--- worker/workers/__init__.py
+++ worker/workers/__init__.py
@@ -72,12 +72,15 @@ class Worker(object):
 
 
 class VMWorker(Worker):
-    def __init__(self, disk="/data/cgc-vm.qcow2", kvm_timeout=5, sandbox=True, ssh_port=8022,
-                 ssh_username="root", ssh_keyfile="/data/cgc-vm.key", vm_name=None):
+    def __init__(self, disk="/data/cgc-vm.qcow2", kvm_timeout=5, restrict_net=False, sandbox=True,
+                 snapshot=True, ssh_port=8022, ssh_username="root", ssh_keyfile="/data/cgc-vm.key",
+                 vm_name=None):
         super(Worker, self).__init__()
         self._disk = disk
         self._kvm_timeout = kvm_timeout
+        self._restrict_net = 'on' if restrict_net else 'off'
         self._sandbox = 'on' if sandbox else 'off'
+        self._snapshot = 'on' if snapshot else 'off'
         self._ssh_port = ssh_port
         self._ssh_username = ssh_username
         self._ssh_keyfile = ssh_keyfile
