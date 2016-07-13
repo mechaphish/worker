021091331e13daace52b03056b437a069c628493
Kevin Borgolte <cao@cao.vc>
2016-07-12 17:10:11 -0700

    Reorder imports and add Tester imports

--- worker/workers/tester.py
+++ worker/workers/tester.py
@@ -11,69 +11,11 @@ import worker.workers
 LOG = worker.workers.LOG.getChild('test')
 LOG.setLevel('DEBUG')
 
-LOITER_TIME = 300  # 5 - minutes
-MAX_RETRY_TIMES = 10000  # maximum number of times to try for jobs to run
-
-
-class CRSAPIWrapper:
-    def __init__(self):
-        # TODO: some common stuff
-        pass
-
-    def get_testjobs(self):
-        """
-        :return:
-        """
-        all_jobs = []
-        for b in crscommon.api.get_all_binaries():
-            for testcase_id in crscommon.api.get_testcases_for_testing(b.ct.id, b.binary_id):
-                all_jobs.append(TesterJob(b, testcase_id))
-        return all_jobs
-
-    def mark_testjob_busy(self, busy_test_job):
-        """
-        :param busy_test_job:
-        :return:
-        """
-        return crscommon.api.mark_testcase_busy(busy_test_job.binary.ct.ct_id, busy_test_job.binary.binary_id,
-                                                busy_test_job.testcase_id)
-
 
 class TestWorker(worker.workers.Worker):
     def __init__(self, number_of_vms=2):
-        super(TestWorker, self).__init__()
-        self.max_vms = number_of_vms
+        super(self.__class__, self).__init__()
         self.free_vms = set()
-        # TODO: Change this
-        self.crshelper = CRSAPIWrapper()
-        # Lock to access free VMs.
-        self.vm_lock = threading.Lock()
-        # TODO: Set up those number of VMs
-
-    def get_free_vm(self):
-        """
-        :return:
-        """
-        to_ret = None
-        if self.free_vms is not None:
-            with self.vm_lock:
-                if len(self.free_vms) > 0:
-                    to_ret = self.free_vms.pop()
-        else:
-            LOG.error("Free VMs member is None. This should never happen.")
-        return to_ret
-
-    def put_free_vm(self, to_free_vm):
-        """
-
-        :param to_free_vm:
-        :return:
-        """
-        if to_free_vm is not None:
-            with self.vm_lock:
-                if self.free_vms is None:
-                    self.free_vms = set()
-                self.free_vms.add(to_free_vm)
 
     @staticmethod
     def parse_cb_test_out(output_buf):
@@ -106,24 +48,17 @@ class TestWorker(worker.workers.Worker):
 
         return final_result, performance_json
 
-    def _log_run(self, job, test_vm):
-        """
-        :param job:
-        :param test_vm:
-        :return:
-        """
-        # TODO: finish this
-        LOG.debug("Trying to run job:" + str(job) + " on VM:" + str(test_vm))
-        # Run Job
-        # Update the result to do.
-        pass
-
     def _run(self, job):
-        """
+        LOG.debug("Starting Tester worker")
 
-        :param job:
-        :return: None
-        """
+        # Create VM
+
+        # Wait for SSH
+
+        # Spawn worker inside of VM
+
+
+
         try:
             LOG.debug("Starting Test Worker")
             # Get the free VM.
