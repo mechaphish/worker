f091bcd10ea317a196d5604cfa174698867c10f4
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-07-21 21:57:02 -0700

    Fixing assert

--- worker/workers/patch_performance.py
+++ worker/workers/patch_performance.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from patch_performance import compute_patch_performance
+from farnsworth.models import PatchPerformanceJob
 
 import worker.workers
 LOG = worker.workers.LOG.getChild('patch_performance')
@@ -21,7 +22,7 @@ class PatchPerformanceWorker(worker.workers.Worker):
         super(self.__class__, self).__init__()
 
     def _run(self, job):
-        assert isinstance(job, PatchPerformanceWorker)
+        assert isinstance(job, PatchPerformanceJob)
 
         target_cs = job.cs
         if target_cs is not None:
